82d2eba53c567e4ddf00a3b7adc4e91b3c9b5ede
Nardus Mollentze <nmollentze@gmail.com>
2022-12-15 12:41:56 +0000

    Minor refactor

--- src/beast/__init__.py
+++ src/beast/__init__.py
@@ -1,10 +1,8 @@
 # Functions for updating or modifying BEAST xml files
 
 # Expose commonly-used functions directly:
-from .update_from_template import update_from_template
+from .templating import update_from_template
 
-from .add_taxon import add_taxon, add_unsampled_taxon
-from .add_taxon_attributes import add_taxon_attributes
+from .taxon import add_taxon, add_unsampled_taxon
+from .taxon_attributes import add_taxon_attributes
 from .predictor import add_predictor
-
-from .set_run_length import set_run_length
--- src/beast/utils.py
+++ src/beast/utils.py
@@ -25,3 +25,34 @@ def as_decimal_year(date):
     fraction = elapsed / duration
 
     return year + fraction
+
+
+def set_run_length(xml_root, run_length, n_samples=10000):
+    """
+    Update a BEAST xml to run for a given number of MCMC steps, adjusting log frequency to match. 
+    This will update both the <mcmc> element and all <log> and <logTree> elements.
+    
+    `xml_root` will be modified in-place.
+    
+    Parameters
+    ----------
+    xml_root : xml.etree.ElementTree.Element
+        The root element of the xml file.
+    run_length : int
+        The number of MCMC steps required.
+    n_samples : int
+        The number of samples to take (default: 10 000). This is used to set the log frequency.
+    """
+    log_frequency = run_length // n_samples
+
+    # Update <mcmc> element:
+    mcmc = xml_root.mcmc
+    mcmc.set("chainLength", str(run_length))
+
+    # Update all <log> elements:
+    for log in mcmc.iterchildren("log"):
+        log.set("logEvery", str(log_frequency))
+
+    # Update all <logTree> elements:
+    for log in mcmc.iterchildren("logTree"):
+        log.set("logEvery", str(log_frequency))
